c110913efbeeace8837ea77d46022c6a76b8ce9c
nara <dlskfksms@hanmail.net>
2024-03-16 20:32:58 +0900

    ㅇㅇㅇㅇ

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const filepath = path.join(__dirname, 'letters/letter-data.json');
+  fs.readFile(filepath, (err, data) => {
+    if (err) {
+      res.render('index');
+    } else {
+      try{
+        data = JSON.parse(data).senderList;
+        res.render('index', { letterData: data });
+      } catch (e) {
+        res.render('index');
+      }
+
+    }
+  });
 });
+
+router.post('/letter', function(req, res, next) {
+  const filepath = path.join(__dirname, 'letters/letter-data.json');
+  fs.readFile(filepath, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send({ message: 'Internal Server Error' });
+    } else {
+      const { sender, content } = req.body;
+      let letterData = JSON.parse(data);
+      letterData.senderList.push(sender);
+      letterData.letterList.push({
+        sender : sender,
+        content : content,
+        createAt : new Date()
+      });
+      letterData = JSON.stringify(letterData);
+
+      fs.writeFile(filepath, letterData, (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send({ message: 'Internal Server Error' });
+        } else {
+          res.send({ message: 'Letter saved' });
+        }
+      });
+    }
+  });
+});
+
 
 module.exports = router;
